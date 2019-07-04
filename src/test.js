709e7ef58078421c9557f62d87a81dde0048606d
sansolmom <mia@naver.com>
2019-07-04 13:08:34 +0900

    sample test 추가

--- src/test.js
+++ src/test.js
@@ -56,3 +56,52 @@ for(let key in product) {
 }
 
 alert(output);
+
+
+
+  // 변수를 선언합니다. 
+  var primitiveNumber = 273;   //기본 숫자 자료형
+  var objectNumber = new Number(273);   //객체 
+
+  //메서드 추가 
+ Number.prototype.method = function(){
+     return 'Method on Prototype';
+ }
+ //메서드 실행 
+ var output = '';
+ output += primitiveNumber.method() + '\n';
+ output += objectNumber.method();
+ //출력합니다. 
+ alert(output);
+
+
+ var friends = [{
+  name: 'Anna',
+  books: ['Bible', 'Harry Potter'],
+  age: 21
+}, {
+  name: 'Bob',
+  books: ['War and peace', 'Romeo and Juliet'],
+  age: 26
+}, {
+  name: 'Alice',
+  books: ['The Lord of the Rings', 'The Shining'],
+  age: 18
+}];
+
+var allNames = friends.map.call(friends, function(obj){ return obj.name}); 
+
+
+console.log(allNames);
+
+
+let arr = ['Anna','Bob','Silvia','Anna','James'];
+let result = arr.sort().reduce((accumulator, current) => {
+   const length = accumulator.length
+   if(length == 0 || accumulator[length-1] !== current) {
+     accumulator.push(current)
+   }
+   return accumulator;
+}, []);
+
+console.log(result);
